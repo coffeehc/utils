9739b284b1ac44fc8dd46ef416108012e97ac865
黄川 <huangchuan@51zhangdan.com.cn>
2015-11-15 22:27:34 +0800

    delete service warp

---
+++ service_util.go
@@ -0,0 +1,42 @@
+/**
+ * Created by coffee on 15/11/15.
+ */
+package utils
+import (
+	"os"
+	"syscall"
+	"time"
+	"os/signal"
+	"github.com/coffeehc/logger"
+)
+
+func StartService(service Service) {
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Error("service crash,cause is %s", err)
+		}
+		if service.Stop != nil {
+			service.Stop()
+		}
+		time.Sleep(time.Second)
+	}()
+	if service.Run == nil {
+		panic("没有指定Run方法")
+	}
+	WaitStop()
+}
+
+type Service interface {
+	Run()
+	Stop()
+}
+
+/*
+	wait,一般是可执行函数的最后用于阻止程序退出
+*/
+func WaitStop() {
+	var sigChan = make(chan os.Signal, 3)
+	signal.Notify(sigChan, os.Interrupt, os.Kill, syscall.SIGTERM)
+	sig := <-sigChan
+	logger.Debug("接收到指令:%s,立即关闭程序", sig)
+}
--- utils.go
+++ utils.go
@@ -6,10 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"os/signal"
 	"path/filepath"
-	"syscall"
-	"time"
 )
 
 /*
@@ -19,19 +16,6 @@ func GetAppDir() string {
 	file, _ := exec.LookPath(os.Args[0])
 	path, _ := filepath.Abs(file)
 	return filepath.Dir(path)
-}
-
-/*
-	wait,一般是可执行函数的最后用于阻止程序退出
-*/
-func WaitStop(callBack func()) {
-	var sigChan = make(chan os.Signal, 3)
-	signal.Notify(sigChan, os.Interrupt, os.Kill, syscall.SIGTERM)
-	<-sigChan
-	if callBack != nil {
-		callBack()
-	}
-	time.Sleep(time.Second)
 }
 
 /**
